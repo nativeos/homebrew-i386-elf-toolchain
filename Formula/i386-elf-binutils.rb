e21cf67c5731a622cac6095bc7857e1adaa6f165
Dani Rodríguez <dani@danirod.es>
2017-04-14 22:07:04 +0200

    Add i386-elf-binutils

---
+++ Formula/i386-elf-binutils.rb
@@ -0,0 +1,25 @@
+class I386ElfBinutils < Formula
+  desc "GNU Binutils targetting i386-elf"
+  homepage "https://www.gnu.org/software/binutils/"
+  url "http://ftpmirror.gnu.org/binutils/binutils-2.25.1.tar.gz"
+  version "2.25.1"
+  sha256 "82a40a37b13a12facb36ac7e87846475a1d80f2e63467b1b8d63ec8b6a2b63fc"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    mkdir "binutils-build" do
+      system "../configure", "--prefix=#{prefix}",
+                             "--target=i386-elf",
+                             "--disable-multilib",
+                             "--disable-nls",
+                             "--disable-werror"
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "i386-elf-as", "--version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# i386-elf-toolchain tap
+
+This is a tap for Homebrew containing a few tools required for building,
+running and testing i386-elf binaries such as 32 bit operating system kernels
+based on the ELF format, like NativeOS - although it may suit to build and
+test other kernels.
+
+Please, note that at the moment this tap has only been tested on Homebrew for
+MacOS X. I don't know whether this will work with Linuxbrew.
+
+## How to use
+
+Tap the repository and install the packages.
+
+    brew tap nativeos/i386-elf-toolchain
+    brew install i386-elf-binutils
