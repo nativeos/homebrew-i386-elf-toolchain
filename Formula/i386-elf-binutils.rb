d488d524000d332388a43c44e1834ae1bf2c2274
Dani Rodríguez <dani@danirod.es>
2018-01-27 21:49:31 +0100

    Update binutils to 2.30

--- Formula/i386-elf-binutils.rb
+++ Formula/i386-elf-binutils.rb
@@ -1,9 +1,9 @@
 class I386ElfBinutils < Formula
   desc "GNU Binutils targetting i386-elf"
   homepage "https://www.gnu.org/software/binutils/"
-  url "http://ftpmirror.gnu.org/binutils/binutils-2.29.tar.bz2"
-  version "2.29"
-  sha256 "29a29549869039aad75fdf507ac30366da5ad0b974fbff4a8e7148dbf4f40ebf"
+  url "https://sourceware.org/pub/binutils/releases/binutils-2.30.tar.xz"
+  version "2.30"
+  sha256 "6e46b8aeae2f727a36f0bd9505e405768a72218f1796f0d09757d45209871ae6"
 
   def install
     mkdir "binutils-build" do
