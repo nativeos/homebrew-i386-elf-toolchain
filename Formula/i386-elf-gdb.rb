6dfdda68d9aa9074e8077685742d780e011c0516
Dani Rodríguez <dani@danirod.es>
2018-01-31 22:16:00 +0100

    Update GDB to 8.1

--- Formula/i386-elf-gdb.rb
+++ Formula/i386-elf-gdb.rb
@@ -1,9 +1,9 @@
 class I386ElfGdb < Formula
   desc "GNU Project Debugger targetting i386-elf"
   homepage "https://www.gnu.org/software/gdb/"
-  url "http://ftpmirror.gnu.org/gnu/gdb/gdb-8.0.1.tar.xz"
-  version "8.0.1"
-  sha256 "3dbd5f93e36ba2815ad0efab030dcd0c7b211d7b353a40a53f4c02d7d56295e3"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-8.1.tar.xz"
+  version "8.1"
+  sha256 "af61a0263858e69c5dce51eab26662ff3d2ad9aa68da9583e8143b5426be4b34"
 
   def install
     system "./configure", "--prefix=#{prefix}",
